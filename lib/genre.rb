4f2be6dab1b69a58e9b99594af1638383eec8667
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-16 23:38:10 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,10 +1,23 @@
 class Genre
   extend Concerns::Findable
+  extend Concerns::Memorable::ClassMethods
+  include Concerns::Memorable::InstanceMethods
 
-  attr_accessor :artists, :songs
+  attr_accessor :name, :songs
   @@all = []
-  def initialize(songs)
+
+  def initialize(name)
     @name = name
+    @songs = []
+    super()
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artists
+    @songs.collect{|s| s.artist}.uniq
   end
 
   def add_song(song)
