1309fa43dd810dbd1f747b9b75afea40b5373316
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-16 23:37:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,33 @@
-class Song
+class Artist
   extend Concerns::Findable
-  
-  attr_accessor :name, :genres
+  extend Concerns::Memorable::ClassMethods
+  include Concerns::Memorable::InstanceMethods
+
+  attr_accessor :name, :songs
+
   @@all = []
+
   def initialize(name)
     @name = name
+    @songs = []
+    super()
+  end
+
+  def self.all
+    @@all
+  end
+
+  def genres
+    @songs.collect{|s| s.genre}.uniq
   end
 
   def add_song(song)
+    unless self.songs.include?(song)
+      @songs << song
+    end
+    if song.artist.nil?
+      song.artist = self
+    end
+    self
   end
-
 end
