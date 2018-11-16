0c2ddca19807d309db4d5d4e214902d8f324ea5d
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-16 23:35:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,36 @@
 class Song
   extend Concerns::Findable
-  attr_accessor :name, :artist
+  extend Concerns::Memorable::ClassMethods
+  include Concerns::Memorable::InstanceMethods
+
+  attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(name, artist:, genre:)
+  def initialize(name, artist=nil, genre=nil)
     @name = name
+    self.artist = artist unless artist.nil?
+    self.genre = genre unless genre.nil?
+    super()
   end
+
+  def self.all
+    @@all
+  end
+
+  def artist=(artist)
+    @artist = artist
+    unless artist.songs.include?(self)
+      artist.add_song(self)
+    end
+  end
+
+  def genre=(genre)
+    @genre = genre
+    unless genre.songs.include?(self)
+      genre.songs << self
+    end
+  end
+
   def new_from_filename(filename)
     # new song
   end
