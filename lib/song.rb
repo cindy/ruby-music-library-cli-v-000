04cbbb188e69663d4ab5d8bbbe69dd988c194c6e
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-17 00:17:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,7 @@ class Song
 
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_type)
+    require 'pry'; binding.pry 
     new(song_name, artist, genre)
   end
 
