2300fc12f7107524a210c4a2ef433702ab06573a
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-17 00:54:48 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -61,7 +61,7 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     number = gets.to_i
     sorted_songs = Song.all.sort{|a,b| a.name <=> b.name}
-    if number <= sorted_songs.size
+    if number+1 <= sorted_songs.size
       song = sorted_songs[number+1]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
