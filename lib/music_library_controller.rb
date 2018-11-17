d2cdb1a364f97920efba11a784f5b560b69be30c
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-17 00:30:06 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -12,5 +12,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+
+    action = gets
   end
 end
