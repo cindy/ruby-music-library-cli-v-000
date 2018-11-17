a316fbf1ab04f54c6facfe464616510b4ace3e21
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-17 00:30:55 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -4,7 +4,7 @@ class MusicLibraryController
   end
   def call
     action = ""
-    while action != exit do
+    while action != "exit" do
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
